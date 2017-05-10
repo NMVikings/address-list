65402fb309807f023dfbf3f9d256c866baeaa35e
NMVikings <no.more.vikings@gmail.com>
2017-05-10 15:28:47 +0300

    Update data

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ import App from './containers/App';
 import { addAddresses } from './actions';
 import './index.css';
 import data from './data/address-list.json';
-import newData from './data/new_address_list.json';
 
 let store = createStore(addressList, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-store.dispatch(addAddresses([...data, ...newData]));
+store.dispatch(addAddresses(data));
 render(
   <Provider store={store}>
     <App />
